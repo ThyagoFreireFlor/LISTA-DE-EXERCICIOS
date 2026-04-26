f243327d72a476a4b9b168f86e9df9e5e4a9edda
ThyagoFreireFlor <thyagofreire70@gmail.com>
2026-04-25 21:02:39 -0300

    Add files via upload

---
+++ exercício 10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ using namespace std;
+ int main(int argc, char** argv)
+ {
+	 double dolar, real; cotacao;
+	 cout << "Digite a cotacao do dolar";
+	 cin >> cotacao
+	 
+	 cout <<"Digite a quantidade de dolares";
+	 cin >> dolar
+	 
+	 reais = dolar * cotacao;
+	 
+	 cout <<"Seu valor em reais:" << reais;
+	 	 
+	 return 0;
+ }
