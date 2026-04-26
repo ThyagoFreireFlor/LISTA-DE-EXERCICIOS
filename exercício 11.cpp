ebae2f53e2476b2c448e71e5962ca35734b3f156
ThyagoFreireFlor <thyagofreire70@gmail.com>
2026-04-25 21:02:52 -0300

    Add files via upload

---
+++ exercício 11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+ using namespace std;
+ 
+ int main(int argc, char** argv)
+ {
+	 
+ double x, y, resultado; 
+ cout <<"Digite o valor da base";
+ cin >> x;
+ 
+ cout <<"Digite o valor do expoente";
+ cin >> y;
+ 
+ resultado =  pow(x,y);
+ 
+ cout << "Resultado:" << resultado;
+ 
+ return 0;
+ }
+ 
+ 
